c14b5274d2f78b6d20ea7c80f2ac3de8562b3593
NexToDotCom <rizal.developer.blanco@gmail.com>
2023-11-26 23:04:43 +0800

    End of Section 8

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,35 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
 import ActivityList from './ActivityList';
-import ActivityDetails from '../details/ActivityDetails';
-import ActivityForm from '../form/ActivityForm';
 import { useStore } from '../../../stores/store';
 import { observer } from 'mobx-react-lite';
-
-
-interface Props {
-    activities: Activity[];
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 
 export default observer( function ActivityDashboard() {
 
     const {activityStore} = useStore();
-    const {selectedActivity, editMode} = activityStore;
+    const {loadActivities, activityRegistry} = activityStore;
 
-
+    useEffect(() => {
+      if (activityRegistry.size === 0) loadActivities();
+    }, [loadActivities, activityRegistry])
+  
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+  
     return (
         <Grid>
             <Grid.Column width='10'>                
                 <ActivityList />                
             </Grid.Column>
             <Grid.Column width='6'>
-                {selectedActivity && !editMode &&
-                <ActivityDetails />}
-                {editMode &&
-                <ActivityForm/>}
+                <h2>Activity Filters</h2>
             </Grid.Column>
         </Grid>
     )
